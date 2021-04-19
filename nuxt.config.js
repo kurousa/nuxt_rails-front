259d4ef0ca06b480e4296d36ffdb0ac97fbb4f9f
kurousa <r.takahashi.1026@gmail.com>
2021-04-19 21:59:48 +0900

    add_nuxt-i18n-module

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,8 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    'nuxt-i18n',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -65,7 +66,21 @@ export default {
       }
     }
   },
-  
+  // Doc: https://i18n.nuxtjs.org/basic-usage
+  i18n: {
+    locales: ['ja', 'en'],
+    defaultLocale: 'ja',
+    // Doc: https://kazupon.github.io/vue-i18n/api/#properties
+    vueI18n: {
+      fallbackLocale: 'ja',
+      // silentTranslationWarn: true,
+      silentFallbackWarn: true,
+      messages: {
+        ja: require('./locales/ja.json'),
+        en: require('./locales/en.json')
+      }
+    }
+  },  
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
