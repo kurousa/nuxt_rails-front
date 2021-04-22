d1c25ff5829e0801ae37880aa5b972c73d562ba4
kurousa <r.takahashi.1026@gmail.com>
2021-04-23 00:00:59 +0900

    created_top_page_layout

--- plugins/myInject.js
+++ plugins/myInject.js
@@ -2,11 +2,25 @@ class MyInject {
   constructor (app) {
     this.app = app
   }
+
   // i18nページタイトル変換
   pageTitle (routeName) {
     const jsonPath = `pages.${routeName.replace(/-/g, '.')}`
     const title = this.app.i18n.t(jsonPath)
     return (typeof (title) === 'object') ? title.index : title
+  }
+
+  // 日時フォーマット変換
+  dateformat (date) {
+    const dateTimeFormat = new Intl.DateTimeFormat(
+      'ja', { dateStyle: 'medium', timeStyle: 'short' }
+    )
+    return dateTimeFormat.format(new Date(date))
+  }
+
+  // プロジェクトリンク
+  projectLinkTo (id, name = 'project-id-dashboard') {
+    return { name, params: { id } }
   }
 }
 
--- store/index.js
+++ store/index.js
@@ -4,7 +4,17 @@ export const state = () => ({
     beforeLogin: {
       appBarHeight: 56
     }
-  }
+  },
+  current: {
+    project: null
+  },
+  projects: [
+    { id: 1, name: 'MyProject01', updatedAt: '2021-04-22T00:00+09:00' },
+    { id: 2, name: 'MyProject02', updatedAt: '2021-04-22T00:00+09:00' },
+    { id: 3, name: 'MyProject03', updatedAt: '2021-04-24T00:00+09:00' },
+    { id: 4, name: 'MyProject04', updatedAt: '2021-04-25T00:00+09:00' },
+    { id: 5, name: 'MyProject05', updatedAt: '2021-04-26T00:00+09:00' }
+  ]
 })
 
 export const getters = {}
