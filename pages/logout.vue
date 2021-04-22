3c5774bb423118532332853751c22745e88a412f
kurousa <r.takahashi.1026@gmail.com>
2021-04-22 22:58:42 +0900

    add_account_menu_pages_and_logout_page

---
+++ pages/logout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div />
+</template>
+<script>
+export default {
+  async middleware ({ store, redirect, from }) {
+    await store.dispatch('logout')
+    console.log(from.name)
+    if (from.name !== 'index') { return redirect('/') }
+  },
+  layout: 'logout',
+  beforeCreate () {
+    this.$router.replace('/')
+  }
+}
+</script>
